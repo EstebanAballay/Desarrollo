4d5bb9c810ec57afffb8bf647951b1ab8de7e488
Esteban <aballayesteban2@gmail.com>
2025-06-07 17:32:40 -0300

    Recuperar el HelloWorld que decia al principio

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders/orders.controller';
 import { OrdersService } from './orders/orders.service';
 import { Order } from './orders/order.entity';
+import { State } from './orders/state.entity';
+import {AppService} from './app.service';
 
 import { PaymentsController } from './payments/payments.controller';
 import { PaymentsService } from './payments/payments.service';
 
+import { paymentmethod } from './payments/paymentmethod.entity';
+import { transactionDetail } from './payments/transactionDetail.entiy';
+import { transactionStatus } from './payments/transactionstatus.entity';
+import {AppController} from './app.controller'
 
 @Module({
   imports: [
@@ -18,12 +24,12 @@ import { PaymentsService } from './payments/payments.service';
       username: 'postgres',
       password: 'mipassword',
       database: 'ordenes_pagos',
-      entities: [Order], // Se importan las entidades
+      entities: [Order, paymentmethod,transactionDetail, transactionStatus, State], // Se importan las entidades
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Order]), // Se importan entidades para usar en los services
+    TypeOrmModule.forFeature([Order,paymentmethod,transactionDetail,transactionStatus,State]), // Se importan entidades para usar en los services
   ],
-  controllers: [OrdersController, PaymentsController], // Se importan los controladores
-  providers: [OrdersService, PaymentsService], // Se importan los servicios
+  controllers: [OrdersController, PaymentsController,AppController], // Se importan los controladores
+  providers: [OrdersService, PaymentsService,AppService], // Se importan los servicios
 })
 export class AppModule {}
